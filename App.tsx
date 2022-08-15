e907639f7debe2eac1f1961999d18d8f2f98db56
xmd5a <zakaru@gmail.com>
2022-08-15 20:52:15 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -2,5 +2,10 @@ import * as React from 'react';
 import { Section } from './components';
 
 export default function App() {
-  return <Section key={1}>test</Section>;
+  return (
+    <main>
+      <Section key={1}>test</Section>
+      <Section key={2}>test 2</Section>
+    </main>
+  );
 }
--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -7,67 +7,62 @@ type SectionProps = {};
 
 const Section = ({ children }: React.PropsWithChildren<SectionProps>) => {
   const sectionEl = React.useRef<HTMLDivElement>();
-  const isMouseDown = React.useRef(false);
+  const initialMousePosition = React.useRef({ x: 0, y: 0 });
+  const initialSectionPosition = React.useRef({ position: null, x: 0, y: 0 });
   const [isDragged, setIsDragged] = React.useState(false);
-
-  // const onDragStart = (e: React.MouseEvent<HTMLElement>) => {
-  //   console.log(e);
-  // };
-
-  // const onDragEnd = (e: React.MouseEvent<HTMLElement>) => {
-  //   console.log(e);
-  // };
 
   React.useEffect(() => {
     if (isDragged) {
-      console.log('add MouseDown Event');
-
       document.addEventListener('mousemove', onMouseMove);
+
+      console.log('add MouseDown Event');
     }
 
     return () => {
       console.log('unmount me');
-      console.log('remove MouseDown Event');
 
-      document.removeEventListener('mousemove', onMouseMove);
+      if (isDragged) {
+        console.log('remove MouseDown Event');
+        document.removeEventListener('mousemove', onMouseMove);
+      }
     };
   }, [isDragged]);
 
-  // this probably should be in higher component
   const onMouseMove = ({
     pageX: cursorXPosition,
     pageY: cursorYPosition,
   }: MouseEvent) => {
-    console.log(parseToPx(cursorXPosition));
-    console.log(parseToPx(cursorYPosition));
-
-    const { x: sectionElXPosition, y: sectionElYPosition } =
-      sectionEl.current.getBoundingClientRect();
-
-    // console.log(sectionEl.current.getBoundingClientRect());
-
-    console.log('y', parseToPx(cursorYPosition - sectionElYPosition));
-    console.log('x', parseToPx(cursorXPosition - sectionElXPosition));
+    const deltaX = cursorXPosition - initialMousePosition.current.x;
+    const deltaY = cursorYPosition - initialMousePosition.current.y;
 
     sectionEl.current.style.position = 'fixed';
-    sectionEl.current.style.top = parseToPx(
-      cursorYPosition - sectionElYPosition
-    );
     sectionEl.current.style.left = parseToPx(
-      cursorXPosition - sectionElXPosition
+      initialSectionPosition.current.x + deltaX
+    );
+    sectionEl.current.style.top = parseToPx(
+      initialSectionPosition.current.y + deltaY
     );
   };
 
-  const onMouseDown = (e: React.MouseEvent<HTMLElement>) => {
-    console.log('mouse down');
+  const onMouseDown = ({
+    pageX: cursorXPosition,
+    pageY: cursorYPosition,
+  }: React.MouseEvent<HTMLElement>) => {
+    const { x: sectionXPosition, y: sectionYPosition } =
+      sectionEl.current.getBoundingClientRect();
+    initialSectionPosition.current = {
+      x: sectionXPosition,
+      y: sectionYPosition,
+      position: sectionEl.current.style.position,
+    };
+    initialMousePosition.current.x = cursorXPosition;
+    initialMousePosition.current.y = cursorYPosition;
     setIsDragged(true);
-    // isMouseDown.current = true;
   };
 
-  const onMouseUp = (e: React.MouseEvent<HTMLElement>) => {
-    console.log('mouse up');
+  const onMouseUp = () => {
+    sectionEl.current.style.position = initialSectionPosition.current.position;
     setIsDragged(false);
-    // isMouseDown.current = false;
   };
 
   console.log('rerender');
@@ -78,10 +73,6 @@ const Section = ({ children }: React.PropsWithChildren<SectionProps>) => {
       onMouseDown={onMouseDown}
       onMouseUp={onMouseUp}
       ref={sectionEl}
-      // onMouseMove={onMouseMove}
-      // draggable
-      // onDragStart={onDragStart}
-      // onDragEnd={onDragEnd}
     >
       {children}
     </StyledSection>
